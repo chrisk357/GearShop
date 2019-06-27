77ee1e4848727121caa19275b90540430665f8d5
Chris Desktop <chrisk357@gmail.com>
2019-06-27 01:53:58 -0500

    Stoped before adding the view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GearShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GearShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IJerseyRepository _jerseyRepository;
+        private readonly IPantRepository _pantRepository;
+
+        public HomeController(IJerseyRepository jerseyRepository, IPantRepository pantRepository)
+        {
+            _jerseyRepository = jerseyRepository;
+            _pantRepository = pantRepository;
+        }
+        
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
