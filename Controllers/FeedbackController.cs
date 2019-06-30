22021ee51a6fab6711fc79b7d5628f7e5b61a056
Chris Desktop <chrisk357@gmail.com>
2019-06-30 05:35:39 -0500

    Added Authorization to the Feedback Controller

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GearShop.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GearShop.Controllers
 {
+    [Authorize]
     public class FeedbackController : Controller
     {
         private readonly IFeedbackRepository _feedbackRepository;
